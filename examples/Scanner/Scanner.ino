f96d6583fb4d4372d75d29607defcf0cda1de63c
lig@nt <jimkaragian@yahoo.gr>
2019-10-04 22:58:51 +0300

    Update Scanner.ino

--- examples/Scanner/Scanner.ino
+++ examples/Scanner/Scanner.ino
@@ -1,24 +1,30 @@
 /***************************************************
-Copyright (c) 2018 Luis Llamas
-(www.luisllamas.es)
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
+  Copyright (c) 2018 Luis Llamas
+  (www.luisllamas.es)
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
  ****************************************************/
- 
- #include "I2CScanner.h"
+
+#include "I2CScanner.h"
+#include <Wire.h>
+
+//if you use ESP8266-01 with not default SDA and SCL pins, define these 2 lines, else delete them
+#define SDA_PIN 0
+#define SCL_PIN 2
 
 I2CScanner scanner;
 
-void setup() 
+void setup()
 {
-	Serial.begin(9600);
-	while (!Serial) {};
+  Wire.pins(SDA_PIN, SCL_PIN);
+  Serial.begin(9600);
+  while (!Serial) {};
 
-	scanner.Init();
+  scanner.Init();
 }
 
-void loop() 
+void loop()
 {
-	scanner.Scan();
-	delay(5000);
+  scanner.Scan();
+  delay(5000);
 }
